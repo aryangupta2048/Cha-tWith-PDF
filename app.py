dd49d4a8f91b31f66e986f81e690db2429ebd912
aryangupta2048 <aryangupta2048@gmail.com>
2026-08-04 13:25:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ if uploaded_file is not None:
 
     # ================= STEP 5 GET AND LOAD DOCS =================
 
-   documents = load_documents()
+    documents = load_documents()
     embeddings = load_embedding()
     chunks = get_splitted_chunks(documents)
 
